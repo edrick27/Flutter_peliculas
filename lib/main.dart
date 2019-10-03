24d1ba26280401b5058b93ec6ff46aa5696dda61
edrick27 <edricklpcr@gmail.com>
2019-10-02 20:21:36 -0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pe lículas',
+      title: 'Películas',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: getRoutes(),
